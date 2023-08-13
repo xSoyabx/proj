f22d3d5dd77dcde0c866dd43c38ab2342009fa25
Alex0215 <97229263+Alex0215@users.noreply.github.com>
2023-08-14 01:07:46 +0530

    Update AdminDashboard.js

--- E Commerce/client/src/pages/admin/AdminDashboard.js
+++ E Commerce/client/src/pages/admin/AdminDashboard.js
@@ -6,11 +6,13 @@ import AdminMenu from "../../components/layout/AdminMenu";
 const AdminDashboard = () => {
   return (
     <Layout>
-      <div className="conatiner-fluid">
-        <div className="col-md-3">
-          <AdminMenu />
+      <div className="conatiner-fluid m-3 p3">
+        <div className="row">
+          <div className="col-md-3">
+           <AdminMenu />
+          </div>
+          <div className="col-md-9">content</div>
         </div>
-        <div className="col-md-9">content</div>
       </div>
     </Layout>
   );
