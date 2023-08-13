e867f76b7c7a93145aa02f6089bc35ea8a8d8d5d
Alex0215 <97229263+Alex0215@users.noreply.github.com>
2023-08-14 01:17:57 +0530

    admin dashboard username displayed

--- E Commerce/client/src/pages/admin/AdminDashboard.js
+++ E Commerce/client/src/pages/admin/AdminDashboard.js
@@ -1,9 +1,10 @@
 import React from "react";
-
 import Layout from "../../components/layout/layout";
 import AdminMenu from "../../components/layout/AdminMenu";
+import { useAuth } from "../../context/Auth";
 
 const AdminDashboard = () => {
+  const [auth] = useAuth()
   return (
     <Layout>
       <div className="conatiner-fluid m-3 p3">
@@ -11,7 +12,11 @@ const AdminDashboard = () => {
           <div className="col-md-3">
            <AdminMenu />
           </div>
-          <div className="col-md-9">content</div>
+          <div className="col-md-9">
+            <div className="card">
+              <h1>{auth?.user?.name}</h1>
+            </div>
+          </div>
         </div>
       </div>
     </Layout>
