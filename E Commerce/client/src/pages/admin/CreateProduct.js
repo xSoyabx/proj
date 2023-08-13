d74909eaf6208e12b1105fe7a827440b68fbb5c5
Alex0215 <97229263+Alex0215@users.noreply.github.com>
2023-08-14 01:55:46 +0530

    edited all 3 file of dashboard

--- E Commerce/client/src/pages/admin/CreateProduct.js
+++ E Commerce/client/src/pages/admin/CreateProduct.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import Layout from '../../components/layout/layout';
+import AdminMenu from '../../components/layout/AdminMenu';
 
 const CreateProduct = () => {
     return (
-        <div>
-            <h1>Create Product</h1>
-        </div>
+        <Layout title={"Dashboard - Create Product"}>
+            <div className="conatiner-fluid m-3 p3">
+            <div className="row">
+                <div className="col-md-3">
+                    <AdminMenu/>
+                </div>
+                <div className="col-md-9">
+                    <h1>Create Product</h1>
+                </div>
+            </div>
+            </div>
+        </Layout>
     );
 };
 
--- E Commerce/client/src/pages/admin/User.js
+++ E Commerce/client/src/pages/admin/User.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import Layout from '../../components/layout/layout';
+import AdminMenu from '../../components/layout/AdminMenu';
 
 const Users = () => {
     return (
-        <div>
-            <h1>All Users</h1>
-        </div>
+        <Layout title={"Dashboard - All Users"}>
+            <div className="conatiner-fluid m-3 p3">
+            <div className="row">
+                <div className="col-md-3">
+                    <AdminMenu/>
+                </div>
+                <div className="col-md-9">
+                    <h1>All Users</h1>
+                </div>
+            </div>
+            </div>
+        </Layout>
     );
 };
 
