df1c8799936d04e2eaa3066ac4deba469fb57ad0
Alex0215 <97229263+Alex0215@users.noreply.github.com>
2023-08-14 18:24:34 +0530

    orders file in usermenu created

---
+++ E Commerce/client/src/pages/user/Orders.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Layout from './../../components/layout/layout';
+import UserMenu from '../../components/layout/UserMenu';
+
+const Orders = () => {
+  return (
+    <Layout>
+        <div className="container-fluid p-3 m-3">
+            <div className="row">
+            <div className="col-md-3">
+                <UserMenu/>
+            </div>
+            <div className="col-md-9">
+                <h1>All Orders</h1>
+            </div>
+            </div>
+        </div>
+
+    </Layout>
+  )
+}
+
+export default Orders
