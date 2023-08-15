d8a1fd5c5694f5cce8b029af3c598155c11894f4
Riyagaba1 <126313767+Riyagaba1@users.noreply.github.com>
2023-08-15 10:47:57 +0530

    ..

--- E Commerce/client/src/pages/user/Dashboard.js
+++ E Commerce/client/src/pages/user/Dashboard.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Layout from "../../components/Layout/layout";
 import UserMenu from "../../components/layout/UserMenu";
 import { useAuth } from "../../context/Auth";
+import Layout from "../../components/layout/layout";
 
 
 const Dashboard = () => {
