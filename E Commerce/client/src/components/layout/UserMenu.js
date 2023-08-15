e47e94fb9783010f317d2bbc1fe95776a61efeae
Sarthak-Srivastav <141050953+Sarthak-Srivastav@users.noreply.github.com>
2023-08-15 21:48:21 +0530

    small react icons add

--- E Commerce/client/src/components/layout/UserMenu.js
+++ E Commerce/client/src/components/layout/UserMenu.js
@@ -1,22 +1,30 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { CgProfile } from "react-icons/cg";
+import { BsCart3 } from "react-icons/bs";
 
 const UserMenu = () => {
   return (
     <>
-    <div>
-      <div className="text-center">
-        <div className="list-group">
-          <h4>Dashboard</h4>
-          <NavLink to="/dashboard/user/profile" className="list-group-item list-group-item-action">
-            Profile
-          </NavLink>
-          <NavLink to="/dashboard/user/orders" className="list-group-item list-group-item-action">
-            Orders
-          </NavLink>
+      <div>
+        <div className="text-center">
+          <div className="list-group">
+            <h4>Dashboard</h4>
+            <NavLink
+              to="/dashboard/user/profile"
+              className="list-group-item list-group-item-action"
+            >
+              <CgProfile /> Profile
+            </NavLink>
+            <NavLink
+              to="/dashboard/user/orders"
+              className="list-group-item list-group-item-action"
+            >
+              <BsCart3 /> Orders
+            </NavLink>
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 };
