bad6172adbf9654c2740dc2daed580a77c0fb467
Alex0215 <kr.naman007@gmail.com>
2023-08-17 16:43:04 +0530

    search bar, upload image with preview added in create product

--- E Commerce/client/src/pages/admin/CreateProduct.js
+++ E Commerce/client/src/pages/admin/CreateProduct.js
@@ -11,6 +11,8 @@ const CreateProduct = () => {
     const [name,setName] = useState("")
     const [description,setDescription] = useState("")
     const [price,setPrice] = useState("")
+    const [photo,setPhoto] = useState("")
+    const [category,setCategory] = useState("")
     const [quantity,setQuantity] = useState("")
     const [shipping,setShipping] = useState("")
 
@@ -40,7 +42,34 @@ const CreateProduct = () => {
                 </div>
                 <div className="col-md-9">
                     <h1>Create Product</h1>
-                    
+                    <div className="m-1 w-75">
+                      <Select bordered={false}
+                      placeholder="Select a category"
+                        size="large"
+                        showSearch
+                        className="form-select mb-3" onChange={(value) =>{setCategory(value)}} >
+                        {categories?.map(c => (
+                          <option key={c._id} value={c.name}>{c.name}</option>
+                        ))}
+                        </Select>
+                        <div className="mb-3">
+                          <label className="btn btn-outline-secondary col-md-12">
+                              {photo ? photo.name : "Upload Photo"}
+                            <input type="file" name="photo" accept="image/*" onChange={(e) => setPhoto(e.target.files[0]) } hidden />
+                          </label>
+                        </div>
+                        <div className="mb-3">
+                          {photo && (
+                            <div className="text-center">
+                              <img 
+                              src={URL.createObjectURL(photo)} 
+                              alt="Product-Photo" height={'200px'} 
+                              className="img img-responsive" 
+                              />
+                            </div>
+                          )}
+                        </div>
+                    </div>
                 </div>
             </div>
             </div>
