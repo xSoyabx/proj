968bfb6520dea474dce2375a0998e72d50543dca
Riyagaba1 <126313767+Riyagaba1@users.noreply.github.com>
2023-08-19 22:28:28 +0530

    category page

--- E Commerce/client/src/components/layout/header.js
+++ E Commerce/client/src/components/layout/header.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { useAuth } from "../../context/Auth";
 import toast from "react-hot-toast";
 // import logo from "../../image/helmet.png";
 import "../../fonts/Lost_Treasure.otf";
 import SearchInput from "../forms/SearchInput";
+import useCategory from "../../hooks/useCategory";
 
 const Header = () => {
   const [auth, setAuth] = useAuth();
+  const categories = useCategory()
   const handleLogout = () => {
     setAuth({
       ...auth,
@@ -67,16 +69,33 @@ const Header = () => {
                     Home
                   </NavLink>
                 </li>
-                <li className="nav-item">
-                  <NavLink
-                    to="/category"
-                    className="nav-link" /* i have removed active from here . it is suppose to be (nav-link active) */
-                    aria-current="page"
-                    href="#"
-                  >
-                    Category
-                  </NavLink>
-                </li>
+             <li className="nav-item dropdown">
+  <Link className="nav-link dropdown-toggle"
+   to={"/categories"}  
+   data-bs-toggle="dropdown" 
+   >
+    Categories
+  </Link>
+  <ul className="dropdown-menu">
+    <li>
+    <Link className="dropdown-item" 
+            to={"/categories"}  >
+            All Categories
+            </Link>
+    </li>
+  {categories?.map((c)=>(
+          <li>
+            
+            <Link className="dropdown-item" 
+            to={`/category/${c.slug}`}>
+            {c.name}
+            </Link>
+            </li>
+        
+  ))}
+  </ul>
+</li>
+
                 {!auth?.user ? (
                   <>
                     <li className="nav-item">
