ae80d9d45d7e0db5570f0fce5f3fadeca13ee018
Alex0215 <kr.naman007@gmail.com>
2023-08-21 02:07:57 +0530

    Somethings added in footer (working)

--- E Commerce/client/src/components/layout/footer.js
+++ E Commerce/client/src/components/layout/footer.js
@@ -4,90 +4,115 @@ import { Link, NavLink } from "react-router-dom";
 // import"//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js";
 // import "cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js";
 import Policy from "./../../pages/Policy";
+import { BiMailSend, BiPhoneCall, BiSupport } from "react-icons/bi";
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa";
 import {BiSolidUpArrow} from "react-icons/bi";
 
 const Footer = () => {
   return (
     <div>
-        {/* <div className="container-footer"> */}
-            <div className="col-md-14 col-lg-13">
-              <div className="container-footer">
-                <NavLink to="/" style={{ textDecoration: "none" }}>
-                  <span
-                    className="footer-logo"
-                    style={{
-                      color: "Red",
-                      fontSize: "29px",
-                      fontFamily: "lost-treasure",
-                      cursor: "pointer",
-                      marginLeft: "20px",
-                    }}
+      {/* <div className="container-footer"> */}
+      <div className="col-md-14 col-lg-13">
+        <div className="container-footer">
+          <NavLink to="/" style={{ textDecoration: "none" }}>
+            <span
+              className="footer-logo"
+              style={{
+                color: "Red",
+                fontSize: "29px",
+                fontFamily: "lost-treasure",
+                cursor: "pointer",
+                marginLeft: "40px",
+              }}
+            >
+              Rohini Helmets
+            </span>
+          </NavLink>
+          <p className="footer-info-text">
+            Reference site about Lorem Ipsum, giving information on its origins,
+            as well as a random Lipsum generator.
+          </p>
+          <div className="col-md-12">
+            <div className="footer-menu">
+              <ul>
+                <li>
+                  <NavLink to="/" className="nav-link" aria-current="page">
+                    Home
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="#" className="nav-link" aria-current="page">
+                    Terms
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/policy"
+                    className="nav-link"
+                    aria-current="page"
                   >
-                    Rohini Helmets
-                  </span>
-                </NavLink>
-                <p className="footer-info-text">
-                  Reference site about Lorem Ipsum, giving information on its
-                  origins, as well as a random Lipsum generator.
-                </p>
-                {/* <div className="footer-social-link">
-                  <h3>Follow us</h3>
-                  <ul>
-                    <li>
-                      <a href="#">
-                        <i className="fa fa-facebook"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <i className="fa fa-twitter"></i>
-                      </a>
-                    </li>
-                  </ul>
-                </div> */}
-              </div>
-            {/* </div> */}
-          {/* </div> */}
-      </div>
-
-      <div className="copyright">
-        <div className="container-end-footer">
-          <div className="row">
-            <div className="col-md-6">
-              <span>Copyright © 2019, All Right Reserved Seobin</span>
+                    Privacy Policy
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/contact"
+                    className="nav-link"
+                    aria-current="page"
+                  >
+                    Contact
+                  </NavLink>
+                </li>
+              </ul>
             </div>
-            <div className="col-md-6">
-              <div className="copyright-menu">
-                <ul>
+            <div className="footer-social-link">
+              <h3>Follow us</h3>
+              <ul>
+                <li>
+                  <Link
+                    className="footer-social-link facebook"
+                    to="https://www.facebook.com/HelmetMusic/"
+                  >
+                    <FaFacebook /> : Facebook
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="footer-social-link instagram"
+                    to="https://www.instagram.com/helmets_for_india/?hl=en"
+                  >
+                    <FaInstagram /> : Instagram
+                  </Link>
                   <li>
-                    <NavLink to="/" className="nav-link" aria-current="page">
-                      Home
-                    </NavLink>
+                    <Link className="footer-social-link whatsapp" 
+                          to="Whatsapp">
+                      <FaWhatsapp /> : Whatsapp
+                    </Link>
                   </li>
                   <li>
-                    <NavLink to="#" className="nav-link" aria-current="page">
-                      Terms
-                    </NavLink>
+                    <BiMailSend /> : dummy@email.com
                   </li>
                   <li>
-                    <NavLink
-                      to="/policy"
-                      className="nav-link"
-                      aria-current="page"
-                    >
-                      Privacy Policy
-                    </NavLink>
+                    <BiPhoneCall /> : 012-3456789
                   </li>
                   <li>
-                    <NavLink
-                      to="/contact"
-                      className="nav-link"
-                      aria-current="page"
-                    >
-                      Contact
-                    </NavLink>
+                    <BiSupport /> : 1800-0000-0000 (toll free)
                   </li>
-                </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+          {/* </div> */}
+          {/* </div> */}
+        </div>
+
+        <div className="copyright">
+          <div className="container-end-footer">
+            <div className="row">
+              <div className="col-md-12">
+                <span>Copyright © 2019, All Right Reserved Seobin</span>
               </div>
             </div>
           </div>
@@ -100,11 +125,10 @@ const Footer = () => {
           title="Back to Top"
           style={{ display: "block" }}
         >
-          <BiSolidUpArrow/>
+          <BiSolidUpArrow />
         </button>
       </div>
     </div>
-    
   );
 };
 
