944d76502b417434d65d87c6dd199af4fb61705d
Sarthak-Srivastav <141050953+Sarthak-Srivastav@users.noreply.github.com>
2023-09-03 19:57:25 +0530

    Payment gateway

--- E Commerce/controllers/productController.js
+++ E Commerce/controllers/productController.js
@@ -1,8 +1,21 @@
 import slugify from "slugify";
 import productModel from "../models/productModel.js";
 import fs from "fs";
-import exp from "constants";
+// import exp from "constants";
 import categoryModel from "../models/categoryModel.js";
+import orderModel from "../models/orderModel.js";
+import braintree from "braintree";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// payment gateway
+var gateway = new braintree.BraintreeGateway({
+  environment: braintree.Environment.Sandbox,
+  merchantId: process.env.BRAINTREE_MERCHENT_ID,
+  publicKey: process.env.BRAINTREE_PUBLIC_KEY,
+  privateKey: process.env.BRAINTREE_PRIVATE_KEY,
+});
 
 export const createProductController = async (req, res) => {
   try {
@@ -320,10 +333,10 @@ export const relatedProductController = async (req, res) => {
 };
 
 //get product by category
-export const productCategoryController=async(req,res)=>{
-  try{
-    const category = await categoryModel.findOne({slug:req.params.slug})
-    const products=await productModel.find({category}).populate("category")
+export const productCategoryController = async (req, res) => {
+  try {
+    const category = await categoryModel.findOne({ slug: req.params.slug });
+    const products = await productModel.find({ category }).populate("category");
     res.status(200).send({
       success: true,
       category,
@@ -336,5 +349,55 @@ export const productCategoryController=async(req,res)=>{
       error,
       message: "Error while getting product by category",
     });
+  }
+};
+
+//payment gateway API
+// token
+export const brainTreeTokenController = async (req, res) => {
+  try {
+    gateway.clientToken.generate({}, function (err, response) {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.send(response);
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// payment
+export const brainTreePaymentController = async (req, res) => {
+  try {
+    const { nonce, cart } = req.body;
+    let total = 0;
+    cart.map((i) => {
+      total += i.price;
+    });
+    let newTransaction = gateway.transaction.sale(
+      {
+        amount: total,
+        paymentMethodNonce: nonce,
+        options: {
+          submitForSettlement: true,
+        },
+      },
+      function (error, result) {
+        if (result) {
+          const order = new orderModel({
+            products: cart,
+            payment: result,
+            buyer: req.user._id,
+          }).save();
+          res.json({ ok: true });
+        } else {
+          res.status(500).send(error);
+        }
+      }
+    );
+  } catch (error) {
+    console.log(error);
   }
 };
--- E Commerce/routes/productRoutes.js
+++ E Commerce/routes/productRoutes.js
@@ -13,6 +13,8 @@ import {
   serachProductController,
   relatedProductController,
   productCategoryController,
+  brainTreeTokenController,
+  brainTreePaymentController,
 } from "../controllers/productController.js";
 import formidable from "express-formidable";
 const router = express.Router();
@@ -53,15 +55,21 @@ router.post("/product-filters", productFiltersController);
 router.get("/product-count", productCountController);
 
 // product per page
-router.get("/product-list/:page",productListController)
+router.get("/product-list/:page", productListController);
 
 // serach product
-router.get('/search/:keyword',serachProductController)
+router.get("/search/:keyword", serachProductController);
 
 //similar product
-router.get("/related-product/:pid/:cid",relatedProductController)
+router.get("/related-product/:pid/:cid", relatedProductController);
 
 //category wise product
-router.get("/product-category/:slug",productCategoryController)
-export default router;
+router.get("/product-category/:slug", productCategoryController);
 
+//payments routes
+//token
+router.get("/braintree/token", brainTreeTokenController);
+
+//payments
+router.post("/braintree/payment", requireSignIn, brainTreePaymentController);
+export default router;
