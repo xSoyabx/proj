1cb386896313bfdbfda432bb6d5d4b8de7ba071b
Sarthak-Srivastav <141050953+Sarthak-Srivastav@users.noreply.github.com>
2023-09-22 23:02:15 +0530

    ..

--- E Commerce/client/src/components/prices.js
+++ E Commerce/client/src/components/prices.js
@@ -1,37 +1,42 @@
 export const prices = [
   {
     _id: 0,
-    name: "₹0 to 499",
-    array: [0, 499],
+    name: "₹0 to 999",
+    array: [0, 999],
   },
   {
     _id: 1,
-    name: "₹500 to 799",
-    array: [500, 799],
+    name: "₹1000 to 1999",
+    array: [1000, 1999],
   },
   {
     _id: 2,
-    name: "800 to 1099",
-    array: [800, 1099],
+    name: "₹2000 to 2999",
+    array: [2000, 2999],
   },
   {
     _id: 3,
-    name: "1100 to 1399",
-    array: [1100, 1399],
+    name: "₹3000 to 3999",
+    array: [3000, 3999],
   },
   {
     _id: 4,
-    name: "₹1400 to 1699",
-    array: [1400, 1699],
+    name: "₹4000 to 4999",
+    array: [4000, 4999],
   },
   {
     _id: 5,
-    name: "1700 to 1999",
-    array: [1700, 1999],
+    name: "₹5000 to 5999",
+    array: [5000, 5999],
   },
   {
     _id: 6,
-    name: "₹2000 or more",
-    array: [2000, 99999],
+    name: "₹6000 to 6999",
+    array: [6000, 6999],
+  },
+  {
+    _id: 7,
+    name: "₹7000 or more",
+    array: [7000, 99999],
   },
 ];
--- E Commerce/client/src/pages/About.js
+++ E Commerce/client/src/pages/About.js
@@ -16,31 +16,31 @@ const About = () => {
           <h1 className="c-text bg-dark p-2 text-white text-center">
             ABOUT US
           </h1>
-          <p className="text-justify mt-3">
+          <p className="text-white text-justify mt-3">
             <b>
               ALL TYPES OF HELMETS AND RIDING ACCESSORIES ARE AVAILABLE IN YOUR
               REGION AT REASONABLE PRICES.
             </b>
           </p>
-          <p className="text-justify mt-2">
+          <p className="text-white text-justify mt-2">
             WE DEAL IN STUDDS , SMK, STEELBIRD, VEGA, AXOR, ASCONE,MT, GLIDERS ,
             BSDDP, PROBIKER AND MORE BRANDS FOR HELMETS AND ACCESSORIES
           </p>
-          <p className="text-justify mt-2">
+          <p className="text-white text-justify mt-2">
             <b>TYPES OF HELMETS</b>
           </p>
-          <p className="text-justify mt-2">
+          <p className="text-white text-justify mt-2">
             FULL FACE HELMETS, HALF FACE/OPEN FACE HELMETS,
           </p>
-          <p className="text-justify mt-2">
+          <p className="text-white text-justify mt-2">
             GRAPHIC HELMETS, FLIP UP HELMETS, KIDS HELMETS, WOMEN HELMETS, MEN
             HELMETS, LIGHT WEIGHT HELMETS, ISI MARKED , DOT PROTECTION , ECE
             CERTIFIED HELMETS
           </p>
-          <p className="text-justify mt-2">
+          <p className="text-white text-justify mt-2">
             <b>RIDING ACCESSORIES</b>
           </p>
-          <p className="text-justify mt-2">
+          <p className="text-white text-justify mt-2">
             GLOVES, RIDING JACKETS, BOOTS, SIDE LUGGAGE ARE ALSO AVAILABLE
           </p>
         </div>
