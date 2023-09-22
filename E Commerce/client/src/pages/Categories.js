c32084fd370df66c0e3245827351c5634794f4b1
Sarthak-Srivastav <141050953+Sarthak-Srivastav@users.noreply.github.com>
2023-09-22 23:23:02 +0530

    .

--- E Commerce/client/src/pages/Categories.js
+++ E Commerce/client/src/pages/Categories.js
@@ -9,6 +9,7 @@ const Categories = () => {
     <Layout title={"All Categories"}>
       <div className="container">
         <div className="row">
+        <h1 className="home-text-center mt-5">All Categories</h1>
           {categories.map((c) => (
             <div className="col-md-6 mt-5 mb-3 gx-3 gy-3" key={c._id}>
               <Link to={`/category/${c.slug}`} className="btn-glass">
